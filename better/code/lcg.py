3bbfbfb849e380b9b87d56ce1c4ba01bf6ef86a6
David Mertz <mertz@gnosis.cx>
2023-07-20 14:48:46 -0400

    Current snapshot of Better files

--- better/code/lcg.py
+++ better/code/lcg.py
@@ -1,5 +1,5 @@
 class LinearCongruentialGenerator:
-    def __init__(self, seed: int=123):
+    def __init__(self, seed: int = 123):
         self.__seed: int = seed
         self._multiplier: int = 1103515245
         self._modulus: int = 2**32
@@ -27,3 +27,22 @@ class LinearCongruentialGenerator:
             % self._modulus)
         return self._state / self._modulus
 
+
+def lcg(
+    seed: int = 123,
+    multiplier: int = 1_103_515_245,
+    modulus: int = 2**32,
+    increment: int = 1
+):
+    # Simple constraints we should follow
+    assert 0 < modulus
+    assert 0 < multiplier < modulus
+    assert 0 <= increment < modulus
+    assert 0 <= seed < modulus
+
+    # One initial application of recurrence relation
+    state = (multiplier * seed + increment) % modulus
+
+    while True:
+        state = (multiplier * state + increment) % modulus
+        yield state / modulus
